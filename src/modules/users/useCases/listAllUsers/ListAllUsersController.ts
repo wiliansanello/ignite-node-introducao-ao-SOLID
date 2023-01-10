9e39f1515bfd3f553df399796483c5d08ac0ca01
Wilian Ansanello <wiliansanello@outlook.com>
2023-01-10 13:40:21 -0300

    reescrita da conversão de tipo do parâmetro user_id

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,11 +6,11 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    const { user_id } = request.headers;
-    const user = String(user_id);
+    let { user_id } = request.headers;
+    user_id = String(user_id);
 
     try {
-      const usersList = this.listAllUsersUseCase.execute({ user_id: user });
+      const usersList = this.listAllUsersUseCase.execute({ user_id });
 
       return response.status(200).json(usersList);
     } catch (err) {
